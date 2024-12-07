d3efd9ddbe6ad0f02cc5a7cf08eddc81676dbb97
coder-mizan01 <p.mizan2441@gmail.com>
2024-12-07 19:31:48 +0600

    Revert maint.tsx to the previous version

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,9 @@ import { store } from './App/store.ts';
 import "../src/index.css";
 
 createRoot(document.getElementById('root')!).render(
+  <StrictMode>
       <Provider store={store} >
       <App />
     </Provider>
+  </StrictMode>,
 )
