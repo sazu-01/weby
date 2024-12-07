3207f115fa6362f379c0139bb5c9ddb30eb48ec0
coder-mizan01 <p.mizan2441@gmail.com>
2024-12-07 09:55:39 +0600

    add TemplateEditor to edit TemplateOne

--- client/src/UI_Collection/TemplateTwo/index.tsx
+++ client/src/UI_Collection/TemplateTwo/index.tsx
@@ -1,65 +1,16 @@
-import { FormEvent, useState, useEffect } from "react";
-import { api } from "../../App/apiService";
 
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const TemplateTwo = () => {
-  const [websiteName, setWebsiteName] = useState("");
-  const [professionalTitle, setProfessionalTitle] = useState("");
-  const [documentId, setDocumentId] = useState(""); 
+
+const TemplateTwo = ({ websiteName, professionalTitle }: { 
+  websiteName: string, 
+  professionalTitle: string 
+}) => {
+
   const [isOpen, setIsOpen] = useState(false);
 
 
-  // Fetch website name when component mounts
-  useEffect(() => {
-    const fetchWebsiteData = async () => {
-      try {
-        const response = await api.get("/website/get",{params : {templateId : "p2"}});
-        if (response.data.payload.data && response.data.payload.data.length > 0) {
-          const latestData = response.data.payload.data[0];
-          setWebsiteName(latestData.websiteName);
-          setProfessionalTitle(latestData.professionalTitle);
-          setDocumentId(latestData._id); // Save the document ID for updates
-        }
-      } catch (error) {
-        console.error("Error fetching website name:", error);
-      }
-    };
-
-    fetchWebsiteData();
-  }, []);
-
-       const navigate = useNavigate();
-      //checked 
-      const isUserLoggedIn = localStorage.getItem("isLoggedIn");
-
-      useEffect(()=>{
-        if(!isUserLoggedIn){
-          navigate('/login');
-        }
-      },[isUserLoggedIn, navigate])
-
-  const handleUpdate = async (e: FormEvent) => {
-    e.preventDefault();
-    
-    if (!documentId) {
-      alert("No existing website name found to update!");
-      return;
-    }
-
-    try {
-      const res = await api.put(`/website/update/${documentId}`, 
-      { websiteName, professionalTitle, templateId : "p2" });
-      if (res.status === 200) {
-        setWebsiteName(websiteName);
-        setProfessionalTitle(professionalTitle)
-      }
-    } catch (error) {
-      console.error("Error updating website name:", error);
-      alert("Failed to update website data. Please try again.");
-    }
-  };
-
+  
   return (
     <>
 <header className="bg-white-600 shadow-md">
@@ -139,41 +90,7 @@ const TemplateTwo = () => {
           <p>{professionalTitle || "default title"}</p>
         </div>
       </main>
-      <form
-        onSubmit={handleUpdate}
-        className="text-center mt-12"
-      >
-        <div>
-          <label htmlFor="websiteName" className="mr-2">Update Website Name: </label>
-          <input
-            id="websiteName"
-            type="text"
-            onChange={(e) => setWebsiteName(e.target.value)}
-            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-            placeholder={websiteName || "Enter new website name"}
-          />
-        </div>
 
-        <div>
-          <label htmlFor="profesttionTitle" className="mr-2">Update professionalTitle: </label>
-          <input
-            id="profesttion title"
-            type="text"
-            onChange={(e) => setProfessionalTitle(e.target.value)}
-            className="px-4 py-2 border border-gray-300 rounded-lg mt-2 focus:outline-none focus:border-indigo-500"
-            placeholder={"enter professional title"}
-          />
-        </div>
-
-
-        <button
-          type="submit"
-          className="bg-yellow-400 py-2 px-4 rounded-md mt-4 hover:bg-yellow-500 transition"
-          disabled={!documentId}
-        >
-          Update Data
-        </button>
-      </form>
     </>
   );
 };
